5cbf956ab0ec311425ef74dbbe8c634b8f8651ce
vitaliiromanko <r.vitalii.v030@gmail.com>
2022-04-16 19:06:44 +0300

    Added the first links between pages and changed some pages

--- app/src/main/java/com/nulp/moonice/BookActivity.kt
+++ app/src/main/java/com/nulp/moonice/BookActivity.kt
@@ -1,9 +1,13 @@
 package com.nulp.moonice
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.google.gson.Gson
 import com.nulp.moonice.databinding.ActivityBookBinding
-import com.nulp.moonice.databinding.ActivityLoginBinding
+import com.nulp.moonice.model.Genre
+import java.text.DateFormat
+import java.text.SimpleDateFormat
+import java.util.*
 
 class BookActivity : AppCompatActivity() {
 
@@ -14,5 +18,21 @@ class BookActivity : AppCompatActivity() {
         bindingClass = ActivityBookBinding.inflate(layoutInflater)
         setContentView(bindingClass.root)
         setSupportActionBar(bindingClass.myToolbar)
+        val gson = Gson()
+        val title = gson.fromJson(intent.getStringExtra("title"), String::class.java)
+        val author = gson.fromJson(intent.getStringExtra("author"), String::class.java)
+        val uploadDate = gson.fromJson(intent.getStringExtra("uploadDate"), Date::class.java)
+        val description = gson.fromJson(intent.getStringExtra("description"), String()::class.java)
+        val genre = gson.fromJson(intent.getStringExtra("genre"), Genre::class.java)
+        //val genre = gson.fromJson(intent.getStringExtra("genre"), Genre::class.java)
+        val dateFormat: DateFormat = SimpleDateFormat("yyyy")
+        val strDate = dateFormat.format(uploadDate)
+
+        bindingClass.bookTitleMain.text = title
+        bindingClass.navHeaderBook.bookTitleHeaderBar.text = title
+        bindingClass.navHeaderBook.bookAuthorHeaderBar.text = "$author $strDate"
+        bindingClass.navHeaderBook.bookDescriptionHeaderBar.text = description
+        bindingClass.navHeaderBook.bookGenreHeaderBar.text = genre.genreName
+
     }
 }
--- app/src/main/java/com/nulp/moonice/ui/books/BooksFragment.kt
+++ app/src/main/java/com/nulp/moonice/ui/books/BooksFragment.kt
@@ -2,18 +2,27 @@ package com.nulp.moonice.ui.books
 
 import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
-import com.nulp.moonice.PlayerActivity
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.google.gson.Gson
+import com.nulp.moonice.*
 import com.nulp.moonice.databinding.FragmentBooksBinding
+import com.nulp.moonice.model.Book
+import com.nulp.moonice.model.BooksListener
+import com.nulp.moonice.model.BooksService
 
 class BooksFragment : Fragment() {
 
     private var _binding: FragmentBooksBinding? = null
+    private lateinit var adapter: BooksAdapter
+
+    private val booksService: BooksService
+        get() = (requireActivity().applicationContext as App).booksService
 
     // This property is only valid between onCreateView and
     // onDestroyView.
@@ -41,12 +50,38 @@ class BooksFragment : Fragment() {
 //            startActivity(intent)
 //        }
 
-
+        initRecycleView(binding)
         return root
+    }
+
+    private fun initRecycleView(binding: FragmentBooksBinding) {
+        adapter = BooksAdapter(object : BooksActionListener {
+            override fun onBookClick(book: Book) {
+                Log.d("Test", "Clicked")
+                //Toast.makeText(this@MainActivity, "Book: ${book.title}", Toast.LENGTH_SHORT).show()
+                val gson = Gson()
+                val intent = Intent(activity, BookActivity::class.java)
+                intent.putExtra("title", gson.toJson(book.title))
+                intent.putExtra("author", gson.toJson(book.author))
+                intent.putExtra("uploadDate", gson.toJson(book.uploadDate))
+                intent.putExtra("description", gson.toJson(book.description))
+                intent.putExtra("genre", gson.toJson(book.genre))
+                intent.putExtra("audioRecords", gson.toJson(book.audioRecords))
+                startActivity(intent)
+            }
+        })
+        binding.recyclerView.layoutManager = LinearLayoutManager(activity, LinearLayoutManager.HORIZONTAL, false)
+        binding.recyclerView.adapter = adapter
+        booksService.addListener(booksListener)
     }
 
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
+        booksService.removeListener(booksListener)
+    }
+
+    private val booksListener: BooksListener = {
+        adapter.books = it
     }
 }
