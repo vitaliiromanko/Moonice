88a6ba0f4e68a207bcbd1e5cd6ccc9f63c46c751
vitaliiromanko <r.vitalii.v030@gmail.com>
2022-04-20 18:26:19 +0300

    Fixed mistakes

--- app/src/main/java/com/nulp/moonice/utils/FirebaseHelper.kt
+++ app/src/main/java/com/nulp/moonice/utils/FirebaseHelper.kt
@@ -3,7 +3,7 @@ package com.nulp.moonice.utils
 const val FIREBASE_URL = "https://moonicedatabase-default-rtdb.europe-west1.firebasedatabase.app"
 
 const val NODE_USERS = "Users"
-const val NODE_USER_DETAILS = "UserDetails"
+const val NODE_USER_DETAILS = "UsersInfo"
 const val NODE_USERNAMES = "Usernames"
 
 const val USER_DETAILS_UID = "uid"
