78ebd874a8557990db74b64bc407a82cda49d348
vouzze <voslavuhristu@gmail.com>
2022-05-01 18:11:44 +0300

    error settings, vitalya help

--- app/src/main/java/com/nulp/moonice/ui/MainActivity.kt
+++ app/src/main/java/com/nulp/moonice/ui/MainActivity.kt
@@ -63,7 +63,7 @@ class MainActivity : AppCompatActivity() {
         // menu should be considered as top level destinations.
         appBarConfiguration = AppBarConfiguration(
             setOf(
-                R.id.nav_books, R.id.nav_bookmarks
+                R.id.nav_books, R.id.nav_bookmarks, R.id.nav_settings
             ), drawerLayout
         )
 
